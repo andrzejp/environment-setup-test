c881e8254851e2754da89336a5025fd1fabf1fbe
Andrzej Prochyra <andrzej@prochyra.name>
2020-11-21 16:19:27 +0000

    Initial commit

---
+++ src/test/java/io/prochyra/ASingleTest.java
@@ -0,0 +1,13 @@
+package io.prochyra;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+class ASingleTest {
+
+    @Test
+    void fail_the_test() {
+        fail("This test fails, so the setup works!");
+    }
+}
